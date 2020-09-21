69aca1c21a8d883a82850bfafacdea6b6e037d77
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2020-09-21 14:37:58 +0200

    Add linting

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    'browser': true,
+    'es2020': true,
+    'node': true,
+    'jest/globals': true,
+    'cypress/globals': true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react', 'jest'],
+  rules: {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': [
+      'error',
+      {
+        before: true,
+        after: true,
+      },
+    ],
+  },
+}
