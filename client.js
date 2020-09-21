26da68befe5f59c2e99afca9e5fe7e4c9cd1d627
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2020-09-21 16:03:41 +0200

    Show messages

--- client.js
+++ client.js
@@ -2,6 +2,24 @@ import io from 'socket.io-client'
 
 const socket = io('http://localhost:3000')
 
-socket.on('connect', () => console.info('Connected'))
+const newMessage = document.getElementById('new-message')
+const allMessages = document.getElementById('all-messages')
+
+socket.on('connect', () => {
+  socket.on('chat message', (message) => {
+    const addedMessage = document.createElement('p')
+
+    addedMessage.textContent = message
+
+    allMessages.append(addedMessage)
+  })
+
+  newMessage.addEventListener('keyup', (event) => {
+    if (event.code === 'Enter') {
+      socket.emit('chat message', event.target.value)
+      event.target.value = ''
+    }
+  })
+})
 
 socket.on('disconnect', () => console.info('Disconnected!'))
